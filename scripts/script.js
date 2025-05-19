c835a14661c9cfa57d255700a82c44f2c00d04e1
NguyenQuangTri <nguyenquangt892@gmail.com>
2025-05-19 14:54:11 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+﻿//Hàm tự chạy sau khi trang sẵn sàng (hàm ready)
+$(function () {
+    //Khởi tạo tính năng AOS animation
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-out-back',
+        delay: 0,
+    });
+});
